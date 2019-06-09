a1ca5d207ddc08507cfef30bb1c1ffaf02fdc661
Leon Lin <leonlin310@gmail.com>
2019-06-09 17:04:26 -0500

    working starting files

---
+++ data/friends.js
@@ -0,0 +1,34 @@
+//all api POSTs and api GETs will flow to this file
+
+// 5. You should save your application's data inside of `app/data/friends.js` as an array of objects. Each of these objects should roughly follow the format below.
+// ```json
+// {
+//   "name":"Ahmed",
+//   "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+//   "scores":[
+//       5,
+//       1,
+//       4,
+//       4,
+//       5,
+//       1,
+//       2,
+//       5,
+//       4,
+//       1
+//     ]
+// }
+
+
+var profilesArray = [
+    {
+        profileName: "Leon",
+        photo: "https://www.google.com",
+        scores: [
+            5,
+            4
+        ]
+    }
+];
+
+module.exports = profilesArray;
---
+++ server.js
@@ -0,0 +1,31 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//TODO: Leon to create a new object with data to go inside
+
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+// require("./routing/apiRoutes")(app);
+// require("./routing/htmlRoutes")(app);
+
+//boilerplate to listen for 'app'. 
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
+  
